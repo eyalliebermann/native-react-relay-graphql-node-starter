da793de8b4dbdffa51c367d848bb4e9bcb96e75b
eyalliebermann <eyal.liebermann@gmail.com>
2017-07-10 14:52:30 +0300

    serve static index page

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
+app.use('/', express.static('public'))
+
+
+//dynamic paths
+app.get('/hello', function (req, res) {
   res.send('Hello boilerplate world!')
 })
 
+//go listen
 app.listen(PORT, function () {
   console.log(`Express server listens on port ${PORT}!`)
 })
