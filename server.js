6ead9d46f8ee700e3df4d3a4a85d0ba22fc06997
eyalliebermann <eyal.liebermann@gmail.com>
2017-07-10 14:12:18 +0300

    heroku ready hello world

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 
+const PORT = process.env.PORT | 3000;
+
 app.get('/', function (req, res) {
   res.send('Hello boilerplate world!')
 })
 
-app.listen(3000, function () {
-  console.log('Express server listens on hard coded port 3000!')
+app.listen(PORT, function () {
+  console.log(`Express server listens on hard coded port ${PORT}!`)
 })
