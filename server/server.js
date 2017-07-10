986b14148dacab92b738dae584fb7eca46f89cb3
eyalliebermann <eyal.liebermann@gmail.com>
2017-07-10 20:41:51 +0300

    mv all server dependncies to folder to allow transpilation so heroku can run

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const path = require('path');
 
-const {Schema} = require('../data/schema');
+import {Schema} from './data/schema'
 
 const app = express();
 
