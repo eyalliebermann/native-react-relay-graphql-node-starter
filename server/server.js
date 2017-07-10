a817a27dde805a05a9e579e215ec3b41b7334583
eyalliebermann <eyal.liebermann@gmail.com>
2017-07-10 19:03:33 +0300

    move server to directory - Procfile still on root

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const path = require('path');
 
-const {Schema} = require('./data/schema');
+const {Schema} = require('../data/schema');
 
 const app = express();
 
