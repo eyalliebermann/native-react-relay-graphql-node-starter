97c63596076ceb2ae0495d5f1692c65046a7f4d0
eyalliebermann <eyal.liebermann@gmail.com>
2017-07-11 12:14:16 +0300

    no relay-garhql usage but still module - status heroku is up again

--- server/data/schema.js
+++ server/data/schema.js
@@ -1,94 +1,3 @@
-/*
-example query:
-{
-  currentTeam{
-    id
-    tasks{
-      edges{
-        node{
-          id
-          name
-      }
-      node{
-        id
-        name
-      }
-      }
-    }
-  }
-}
-
-example answer:
-{
-  "data": {
-    "currentTeam": {
-      "id": "VGVhbTox",
-      "tasks": {
-        "edges": [
-          {
-            "node": {
-              "id": "VGFzazow",
-              "name": "Answer the phone"
-            }
-          },
-          {
-            "node": {
-              "id": "VGFzazox",
-              "name": "Wash the dishes"
-            }
-          },
-          {
-            "node": {
-              "id": "VGFzazoy",
-              "name": "Throw up a holiday dinner"
-            }
-          }
-        ]
-      }
-    }
-  }
-}
-
-////////////////////
-import {
-  connectionArgs,
-  connectionDefinitions,
-  connectionFromArray,
-  fromGlobalId,
-  globalIdField,
-  mutationWithClientMutationId,
-  nodeDefinitions,
-} from 'graphql-relay';
-
-/**
- * We get the node interface and field from the Relay library.
- *
- * The first method defines the way we resolve an ID to its object.
- * The second defines the way we resolve an object to its GraphQL type.
- *//*
-var {nodeInterface, nodeField} = nodeDefinitions(
-  (globalId) => {
-    var {type, id} = fromGlobalId(globalId);
-    if (type === 'Team') {
-      return getTeam(id);
-    } else if (type === 'Task') {
-      return getTask(id);
-    } else {
-      return null;
-    }
-  },
-  (obj) => {
-    if (obj instanceof Team) {
-      return teamType;
-    } else if (obj instanceof Task)  {
-      return taskType;
-    } else {
-      return null;
-    }
-  }
-);
-*/
-
 import {
   GraphQLBoolean,
   GraphQLFloat,
@@ -100,8 +9,6 @@ import {
   GraphQLSchema,
   GraphQLString,
 } from 'graphql';
-
-
 
 import {
   // Import methods that your schema can use to interact with your database
@@ -127,37 +34,8 @@ var teamType = new GraphQLObjectType({
       type:GraphQLString,
       description:'team identifier',
       resolve: ()=>'1'}
-      //,
-    // first_task:{
-    //   type:Task,
-    //   description:'The first task',
-    //   resolve:()=>getTaskByIndex(0)
-
-    // }
-    //,
-    // tasks: {
-    //   type: GraphQLList(Task),
-    //   description: 'A team\'s collection of tasks',
-    //   resolve: () => getTasks()
-    // },
   })
 });
-
-// var taskType = new GraphQLObjectType({
-//   name: 'Task',
-//   description: 'A shiny task',
-//   fields: () => ({
-//     id: {GraphQLID},
-//     title: {
-//       type: GraphQLString,
-//       description: 'The name of the task',
-//     },
-//     description:{
-//       type:GRaphQLString,
-//       description:'The elaborated description of the task'
-//     }
-//   })
-// });
 
 /**
  * This is the type that will be the root of our query,
